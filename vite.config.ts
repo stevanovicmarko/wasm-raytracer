a54c700d94bfbab0706d1b1ddeda2f2099dc45c2
Marko Stevanović <stevanovicmarko@gmail.com>
2026-01-08 23:21:35 +0100

    Update to latest vite.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   build: {
     minify: false
   },
